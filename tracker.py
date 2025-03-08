0707fe84528319674c7fff19c76ca4b0f80a4dfe
Angella Chinhuru <chnang009@myuct.ac.za>
2025-03-08 14:00:41 +0200

    Included peer tracking system

--- tracker.py
+++ tracker.py
@@ -2,8 +2,9 @@
 import socket
 
 #list of peers available
+peers = []
 
-
+for
 #GeeksforGeeks Schools
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print("UDP Socket created successfully")
@@ -21,9 +22,11 @@ while True:
     #establish connection
     message, addy = serverSocket.recvfrom(1024)
     print("Connection received from", addy, ":", message.decode())
+    peers.append[addy]
     #send 'thank you' to client
     #encode to send byte type
     serverSocket.sendto("Thank you for connecting".encode(), addy)
+    serverSocket.sentto(peers)
     print("Response sent to ", addy)
     #close client connection
 
