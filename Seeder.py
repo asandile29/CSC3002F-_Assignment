e90f6a38e03b2c3230c0463fd06ccf940d1f6fe2
Muelelwa-E <muelelwaelelwani@gmail.com>
2025-03-10 12:33:55 +0200

    updated methods for file transfer

--- Seeder.py
+++ Seeder.py
@@ -11,6 +11,7 @@
 import socket
 import threading
 import time
+import os
 
 class Seeder:
     
@@ -31,17 +32,7 @@ class Seeder:
                self.seeder_IP = seeder_IP
                self.seeder_port = seeder_port
                self.Checkin_Interval = Checkin_Interval
-               self.split_file()
-
-       
-    def split_file(self, file_name, chunk_size = 512 *1024):
-            chunks = {}
-            with open(file_name, "rb") as file:
-                chunk_id = 0
-                while chunk := file.read(chunk_size):  # Read in chunks
-                    self.chunks[chunk_id] = chunk
-                    chunk_id += 1
-            return chunks  # Returns a dictionary {chunk_id: chunk_data}
+               
     
 
     
@@ -59,19 +50,20 @@ class Seeder:
           try:
                   #create a tcp socket
                   TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-                  TCP.bind((self.seeder_IP, self.seeder_port))
+                  # bind to all available interfaces and a specified port
+                  TCP.bind(("0.0.0.0", self.seeder_port))
                   #listen for incoming connections
                   TCP.listen()
                   print(f"Listening to connections from port:{self.seeder_port}...")
 
                   #we want to keep listening for connections
                   while True:
-                        client_sock, client_address = TCP.accept()#accept leecher connection
+                        client_socket, client_address = TCP.accept()#accept leecher connection
                         print(f"Leecher {client_address} connected.")
                         #start new thread to handle the client
-                        thread = threading.Thread(target= self.Attend_clients(), args = (client_sock,client_address))
-                        thread.start()
+                        Client_thread = threading.Thread(target= self.Attend_clients(), args = (client_socket,client_address))
+                        #Client_thread.daemon = True
+                        Client_thread.start()
 
           except Exception as e:
                 print(f"Error: {e}")
@@ -84,36 +76,47 @@ class Seeder:
                 while True:
                       #receive and print client messages
                       request = client_socket.recv(1024).decode("utf-8")
+                      # if request is empty or client requests to close connection
+                      #then seeder responds with closed and connection breaks
                       if not request or request.lower == "close":
                             client_socket.send("closed".encode("utf-8"))
                             break
                       print(f"Received: {request}")
                       # send response to client
                       client_socket.send("Accepted".encode("utf-8"))
-                      try:
-                            chunk_id = int(request) # assume request is a chunk id
-                            self.Send_files(client_socket,chunk_id)
-                      except ValueError:
-                            client_socket.send("Invalid request".encode("utf-8"))
+                       #begin sending files if request is a file_name(*are we sure the client will always request a 
+                       #filename or will there be different instances sometimes and do i need to accomodate that?)
+                      if request:
+                            self.Send_files(request, client_socket)
+                      else:
+                            #respond to client
+                            client_socket.send("Accepted".encode("utf-8"))      
                             
           except Exception as e:
-                print(f"Error when handling client: {e}") 
+                print(f"Error occured while attending to client: {e}") 
           finally:
                 client_socket.close()
                 print(f"Connection to client ({client_address[0]:{client_address[1]}}) closed!") 
 
 
-    def Send_files(self, client_socket, file_name, chunk_id):
+    def Send_files(self, file_name, client_socket):
           #sending the file chunks to the leecher
           try:
-                if chunk_id in self.chunks:
-                       client_socket.send(self.chunks[chunk_id])
-                       print(f"Sent chunk: {chunk_id} to Leecher")
-                else:
-                      client_socket.send(b"Invalid chunk!")
-                         
-          except Exception as e:
-            print(f"Error in sending chunk: {e}")
+                with open(self.file_name, "rb") as file:
+                      while True:
+                            chunk = file.read(1024)# read 1024 bytes at a time(will this ever compromise transfer time if we have a larger file?)
+                            if not chunk:
+                                  break # stop sending chunks if file is transfer is complete
+                            client_socket.send(chunk) #send chunk to client
+                            print(f"Sent {len(chunk)} bytes to leecher.")
+                print(f"File transfer complete!")
+          # if file is not found then show error(should i prompt client to try sending file again?)
+          except FileNotFoundError:
+                
+                error_message = "Error: File not found!"
+                client_socket.send(error_message.encode())
+                print(f"File {file_name} not found!")
+                    
 
 
 
@@ -123,26 +126,26 @@ class Seeder:
             while True:
                   message = f"ALIVE {self.file_name} {self.seeder_port}"
                   UDP_sock.sendto(message.encode(),(self.tracker_IP, self.tracker_port))
-                  print(f"Heartbeat sent!")
+                  print(f"CheckIn sent!")
                   time.sleep(self.Checkin_Interval)# wait before sending notification again
 
 
     def run(self):
-          #Step 1: split file has already been done in __init__
-          #Step 2: Register seeder to tracker
+          
+          #Step 1: Register seeder to tracker
           self.inform_Tracker()
-          #start periodic checkin in a different thread
-          checkin_thread = threading.Thread(target= periodic_CheckIn,daemon = True)
+          #Step 2: start periodic checkin in a different thread
+          checkin_thread = threading.Thread(target= self.periodic_CheckIn,daemon = True)
           checkin_thread.start()
 
           #Step 3: Start TCP server to start file requests
           self.start_Server()
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
            seeder = Seeder(
                   tracker_IP="127.0.0.1",
-                  tracker_port=6000,
-                  seeder_IP="127.0.0.1",
+                  tracker_port=1111,
+                  seeder_IP="128.0.0.1",
                   seeder_port=7000,
                   Checkin_Interval=30
            )
